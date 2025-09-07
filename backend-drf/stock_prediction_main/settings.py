d84e5c63ed38f9ca869122533d06f599a00504c7
milanshit <milanshit941@gmail.com>
2025-09-07 20:46:43 +0530

    registration backend

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "accounts",
+    "api",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,11 @@ WSGI_APPLICATION = "stock_prediction_main.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "StockPrediction",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost"
     }
 }
 
